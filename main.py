d33399eedb99e3ff856f9b22f74dad8b38dcff30
budi <orang2163@gmail.com>
2020-11-05 13:31:48 +0700

    new siap

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from os.path import isfile, join
 import os
 from PIL import Image
 
-man_input = input('masukan nama folder: ')
+man_input = input('masukan nama folder tujuan: ')
+nama_pdf = input('masukan tempat dan nama untuk buku ini!: ')
 main = listdir(r'{}'.format(man_input))
 data = []
 
-
+print(main)
 for i in sorted(main):
-    image = Image.open(r'../{}/{}'.format(man_input,str(i)))
+    print('ini untuk i',i)
+    image = Image.open(r'{}/{}'.format(man_input,str(i)))
     im = image.convert('RGB')
     data.append(im)
 
-im.save(r'book/{}.pdf'.format(man_input), save_all=True, append_images=data)
+im.save(r'{}.pdf'.format(nama_pdf), save_all=True, append_images=data)
